3ee02585a8511c03591e1adeeb94f54b8ad59f7b
Parish Neighbors <pn8bors@gmail.com>
2022-02-08 10:38:45 -0600

    First commit

---
+++ main.js
@@ -0,0 +1,44 @@
+import input from 'input';
+
+function say(num) {
+    switch (num) {
+        case 0:
+            console.log('Zero');
+            break;
+        case 1:
+            console.log('One');
+            break;
+        case 2:
+            console.log('Two');
+            break;
+        case 3:
+            console.log('Three');
+            break;
+        case 4:
+            console.log('Four');
+            break;
+        case 5:
+            console.log('Five');
+            break;
+        case 6:
+            console.log('Six');
+            break;
+        case 7:
+            console.log('Seven');
+            break;
+        case 8:
+            console.log('Eight');
+            break;
+        case 9:
+            console.log('Nine');
+            break;
+    }
+}
+
+async function main() {
+    const yourNum = await input.text('Enter a number');
+    const num = Number(yourNum);
+    say(num);
+}
+
+main();
